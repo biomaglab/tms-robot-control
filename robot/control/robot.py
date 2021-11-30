ed882e064070094d66191fa014bde0759c9ed5b3
Renan <renan_hiroshi@hotmail.com>
2021-11-30 14:27:28 +0200

    ENH: folder refactor

--- robot/control/robot.py
+++ robot/control/robot.py
@@ -1,31 +1,13 @@
-#--------------------------------------------------------------------------
-# Software:     InVesalius - Software de Reconstrucao 3D de Imagens Medicas
-# Copyright:    (C) 2001  Centro de Pesquisas Renato Archer
-# Homepage:     http://www.softwarepublico.gov.br
-# Contact:      invesalius@cti.gov.br
-# License:      GNU - GPL 2 (LICENSE.txt/LICENCA.txt)
-#--------------------------------------------------------------------------
-#    Este programa e software livre; voce pode redistribui-lo e/ou
-#    modifica-lo sob os termos da Licenca Publica Geral GNU, conforme
-#    publicada pela Free Software Foundation; de acordo com a versao 2
-#    da Licenca.
-#
-#    Este programa eh distribuido na expectativa de ser util, mas SEM
-#    QUALQUER GARANTIA; sem mesmo a garantia implicita de
-#    COMERCIALIZACAO ou de ADEQUACAO A QUALQUER PROPOSITO EM
-#    PARTICULAR. Consulte a Licenca Publica Geral GNU para obter mais
-#    detalhes.
-#--------------------------------------------------------------------------
+
 import dataclasses
 import numpy as np
 
-import constants as const
-import transformations as tr
+import robot.constants as const
+import robot.transformations as tr
 
-import robot.elfin as elfin
-import robot.coordinates as coordinates
-import robot.elfin_processing as elfin_process
-
+import robot.control.elfin as elfin
+import robot.control.coordinates as coordinates
+import robot.control.elfin_processing as elfin_process
 
 
 class RobotControl:
@@ -267,8 +249,8 @@ class RobotControl:
 
         coord_head_tracker_in_robot = current_tracker_coordinates_in_robot[1]
         marker_head_flag = markers_flag[1]
-        #coord_obj_tracker_in_robot = current_tracker_coordinates_in_robot[2]
-        #marker_obj_flag = markers_flag[2]
+        coord_obj_tracker_in_robot = current_tracker_coordinates_in_robot[2]
+        marker_obj_flag = markers_flag[2]
         robot_status = False
 
         if self.robot_tracker_flag and np.all(self.m_change_robot_to_head[:3]):
