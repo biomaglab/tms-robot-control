FIX: robot rotation around X and get angles from actual position

robot/control/elfin_processing.py
@@ -414,7 +414,7 @@ class TrackerProcessing:
         M_tracker_in_robot = Y @ target_in_tracker @ tr.inverse_matrix(X)
 
         translation, angles_as_deg = transformation_matrix_to_coordinates(M_tracker_in_robot, axes='rzyx')
-        new_target_in_robot = list(translation) + list(angles_as_deg)
+        new_target_in_robot = list(translation) + list(coord_raw_robot[3:])
 
 
         head_center_coordinates = self.estimate_head_center(tracker_coordinates.m_tracker_to_robot,
@@ -427,6 +427,6 @@ class TrackerProcessing:
         angle = np.rad2deg(np.arccos(np.dot(initaxis, newaxis)))
 
         target_in_robot = new_target_in_robot.copy()
-        target_in_robot[3] = target_in_robot[3] - (angle * sign(crossvec[0]))
+        target_in_robot[5] = target_in_robot[5] - (angle * sign(crossvec[0]))
 
         return compute_robot_to_head_matrix(head_coordinates_in_robot, target_in_robot)

robot/control/robot.py
@@ -329,7 +329,7 @@ class RobotControl:
 
                         #if np.allclose(np.array(new_robot_coordinates)[:3], np.array(current_robot_coordinates)[:3], 0, 0.1):
                            # if self.target_force_sensor * 0.8 < force_sensor_data < self.target_force_sensor * 1.2:
-                        if self.new_force_sensor_data >= (self.target_force_sensor_data + np.abs(self.target_force_sensor_data * 0.2)):
+                        if self.new_force_sensor_data >= (self.target_force_sensor_data + np.abs(self.target_force_sensor_data * (const.ROBOT_FORCE_SENSOR_SCALE_THRESHOLD/100))):
                             self.trck_init_robot.CompensateForce(self.compensate_force_flag)
                             self.compensate_force_flag = True
                             print("Compensatíng Force")