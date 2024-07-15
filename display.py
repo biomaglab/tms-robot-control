35eb7028c7bcb11aeb82feabdf9660dc405e435b
Miika Vuorio <miika.vuorio@aalto.fi>
2024-07-15 16:39:32 +0300

    display z force in display script

--- display.py
+++ display.py
@@ -22,7 +22,7 @@ class PointOfApp:
     def __init__(self):
         # create plots for point of application
         self.fig = plt.figure()
-        self.text = self.fig.text(0.01, 0.01, 'Z-force: ')
+        self.text = self.fig.text(0.01, 0.01, 'Z-force: No data')
         self.ax = self.fig.add_subplot(1, 1, 1)
         self.square = FancyBboxPatch((-5, -5), 10, 10, alpha=0.5, boxstyle="round,pad=3")
         self.ax.add_patch(self.square) 
@@ -50,12 +50,13 @@ class PointOfApp:
                     file.seek(0)
                 read_data = file.readline().decode()
 
-            data = read_data[read_data.find("[")+1:data.find("]")]
+            data = read_data[read_data.find("[")+1:read_data.find("]")]
             data = np.array(data.split(", "), dtype = 'float64')
-            data = data.append(read_data[read_data.find("(")+1:data.find(")")])
+            z_force = read_data[read_data.find("(")+1:read_data.find(")")]
+            self.point.set_data(data[0], data[1])
+            self.text.set_text('Z-force: ' + z_force)
 
             self.point.set_data(data[0], data[1])
-            self.text = "testtest"
 
             # Redraw the plot
             self.fig.canvas.draw()
