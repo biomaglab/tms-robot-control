e31c0db2b555f415137713169f5acb2285222612
Renan Matsuda <renan_hiroshi@hotmail.com>
2026-02-26 09:08:23 +0200

    Revert "FIX: safe height"
    This reverts commit e6dec31d946a1ed8dfa7bc1c15c3f1b8bdeabbc5.

--- robot/control/algorithms/radially_outward.py
+++ robot/control/algorithms/radially_outward.py
@@ -237,7 +237,7 @@ class RadiallyOutwardAlgorithm:
         if not success:
             return False
 
-        pose[2] = self.safe_height.copy()
+        pose[2] = self.safe_height
         success = self.robot.move_linear(
             target=pose,
             speed_ratio=self.default_speed_ratio,
