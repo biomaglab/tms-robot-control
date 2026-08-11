7ae48c04785cce52433f067f4ca63b43f90687f4
Marcio Campos <82606834+MarcioCamposJr@users.noreply.github.com>
2026-08-11 11:52:39 -0300

    ADD: New whitelist messages from navigation messages to accept send to any robot

--- main_loop.py
+++ main_loop.py
@@ -282,16 +282,12 @@ def main(connection=None):
                 for i in range(len(buf)):
                     if topic[i] in const.PUB_MESSAGES:
                         msg_data = buf[i]["data"]
-                        
-                        msg_robot_id = 0
+
                         if isinstance(msg_data, dict):
-                            msg_robot_id = msg_data.get("robot_id", 0)
-                            if msg_robot_id is None:
-                                msg_robot_id = 0
-                            else:
-                                msg_robot_id = int(msg_robot_id)
-                        
-                        if msg_robot_id != robot_id:
+                            msg_robot_id = msg_data.get("robot_id", -1)
+                            if msg_robot_id != robot_id or topic[i] not in const.PUB_MESSAGES_WL:
+                                continue
+                        else:
                             continue
 
                         get_function = {
